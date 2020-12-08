ef1168586cf255be1bf73494d98005c055a1cf9d
Mikene <jonassantos725@gmail.com>
2020-12-08 11:17:21 -0300

    Melhorado o DialogBox

--- lib/res/dialog_box.dart
+++ lib/res/dialog_box.dart
@@ -6,10 +6,10 @@ import 'package:protips/res/strings.dart';
 import 'layouts.dart';
 
 class DialogResult {
-  static int none = 0;
-  static int positive = 1;
-  static int negative = 2;
-  static int aux = 3;
+  static const int none = 0;
+  static const int positive = 1;
+  static const int negative = 2;
+  static const int aux = 3;
 
   DialogResult(this.result);
 
@@ -30,20 +30,64 @@ enum DialogType {
 }
 
 class DialogBox {
-  static Future<DialogResult> dialogSimNao(BuildContext context, {String title, List<Widget> content, EdgeInsets contentPadding}) async {
-    return await _dialogAux(context, title: title, content: content, contentPadding: contentPadding, dialogType: DialogType.simNao);
-  }
-  static Future<DialogResult> dialogCancelOK(BuildContext context, {String title, String auxBtnText, String positiveButton, String negativeButton, List<Widget> content, EdgeInsets contentPadding}) async {
-    return await _dialogAux(context, title: title, positiveButton: positiveButton, negativeButton: negativeButton, auxBtnText: auxBtnText, content: content, contentPadding: contentPadding, dialogType: DialogType.okCancel);
-  }
-  static Future<DialogResult> dialogOK(BuildContext context, {String title, String positiveButton, String auxBtnText, List<Widget> content, EdgeInsets contentPadding}) async {
-    return await _dialogAux(context, title: title, positiveButton: positiveButton, auxBtnText: auxBtnText, content: content, contentPadding: contentPadding, dialogType: DialogType.ok);
-  }
-  static Future<DialogResult> dialogCancel(BuildContext context, {String title, String negativeButton, String auxBtnText, List<Widget> content, EdgeInsets contentPadding}) async {
-    return await _dialogAux(context, title: title, negativeButton: negativeButton, auxBtnText: auxBtnText, content: content, contentPadding: contentPadding, dialogType: DialogType.cancel);
+  static Future<DialogResult> dialogSimNao(BuildContext context,
+      {String title, List<Widget> content, EdgeInsets contentPadding}) async {
+    return await _dialogAux(context,
+        title: title,
+        content: content,
+        contentPadding: contentPadding,
+        dialogType: DialogType.simNao);
   }
 
-  static Future<DialogResult> _dialogAux(BuildContext context, {
+  static Future<DialogResult> dialogCancelOK(BuildContext context,
+      {String title,
+      String auxBtnText,
+      String positiveButton,
+      String negativeButton,
+      List<Widget> content,
+      EdgeInsets contentPadding}) async {
+    return await _dialogAux(context,
+        title: title,
+        positiveButton: positiveButton,
+        negativeButton: negativeButton,
+        auxBtnText: auxBtnText,
+        content: content,
+        contentPadding: contentPadding,
+        dialogType: DialogType.okCancel);
+  }
+
+  static Future<DialogResult> dialogOK(BuildContext context,
+      {String title,
+      String positiveButton,
+      String auxBtnText,
+      List<Widget> content,
+      EdgeInsets contentPadding}) async {
+    return await _dialogAux(context,
+        title: title,
+        positiveButton: positiveButton,
+        auxBtnText: auxBtnText,
+        content: content,
+        contentPadding: contentPadding,
+        dialogType: DialogType.ok);
+  }
+
+  static Future<DialogResult> dialogCancel(BuildContext context,
+      {String title,
+      String negativeButton,
+      String auxBtnText,
+      List<Widget> content,
+      EdgeInsets contentPadding}) async {
+    return await _dialogAux(context,
+        title: title,
+        negativeButton: negativeButton,
+        auxBtnText: auxBtnText,
+        content: content,
+        contentPadding: contentPadding,
+        dialogType: DialogType.cancel);
+  }
+
+  static Future<DialogResult> _dialogAux(
+    BuildContext context, {
     String title,
     String auxBtnText,
     String positiveButton,
@@ -56,11 +100,15 @@ class DialogBox {
     auxBtnText ??= '';
     contentPadding ??= EdgeInsets.fromLTRB(24.0, 20.0, 24.0, 24.0);
 
-    positiveButton ??= (dialogType == DialogType.sim || dialogType == DialogType.simNao) ?
-    MyStrings.SIM : MyStrings.OK;
+    positiveButton ??=
+        (dialogType == DialogType.sim || dialogType == DialogType.simNao)
+            ? MyStrings.SIM
+            : MyStrings.OK;
 
-    negativeButton ??= (dialogType == DialogType.nao || dialogType == DialogType.simNao) ?
-    MyStrings.NAO : MyStrings.CANCELAR;
+    negativeButton ??=
+        (dialogType == DialogType.nao || dialogType == DialogType.simNao)
+            ? MyStrings.NAO
+            : MyStrings.CANCELAR;
 
     bool okButton = _showPositiveButton(dialogType);
     bool cancelButton = _showNegativeButton(dialogType);
@@ -69,34 +117,40 @@ class DialogBox {
     //endregion
 
     return await showModal<DialogResult>(
-      context: context,
-      builder: (context) => StatefulBuilder(
-        builder: (context, setState) => AlertDialog(
-          title: title == null ? null : Text(title),
-          content: SingleChildScrollView(
-              child: Column(
+          context: context,
+          builder: (context) => StatefulBuilder(
+            builder: (context, setState) => AlertDialog(
+              title: title == null ? null : Text(title),
+              content: SingleChildScrollView(
+                  child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: content,
-              )
+              )),
+              contentPadding: contentPadding,
+              actions: [
+                if (auxBtnText.isNotEmpty)
+                  FlatButton(
+                    child: Text(auxBtnText),
+                    onPressed: () =>
+                        Navigator.pop(context, DialogResult(DialogResult.aux)),
+                  ),
+                if (cancelButton)
+                  FlatButton(
+                    child: Text(negativeButton),
+                    onPressed: () => Navigator.pop(
+                        context, DialogResult(DialogResult.negative)),
+                  ),
+                if (okButton)
+                  FlatButton(
+                    child: Text(positiveButton),
+                    onPressed: () => Navigator.pop(
+                        context, DialogResult(DialogResult.positive)),
+                  ),
+              ],
+            ),
           ),
-          contentPadding: contentPadding,
-          actions: [
-            if (auxBtnText.isNotEmpty) FlatButton(
-              child: Text(auxBtnText),
-              onPressed: () => Navigator.pop(context, DialogResult(DialogResult.aux)),
-            ),
-            if (cancelButton) FlatButton(
-              child: Text(negativeButton),
-              onPressed: () => Navigator.pop(context, DialogResult(DialogResult.negative)),
-            ),
-            if (okButton) FlatButton(
-              child: Text(positiveButton),
-              onPressed: () => Navigator.pop(context, DialogResult(DialogResult.positive)),
-            ),
-          ],
-        ),
-      ),
-    ) ?? DialogResult(DialogResult.none);
+        ) ??
+        DialogResult(DialogResult.none);
     // return await showDialog(
     //     context: context,
     //     builder: (context) => AlertDialog(
@@ -122,13 +176,17 @@ class DialogBox {
   }
 
   static bool _showPositiveButton(DialogType dialogType) {
-    return (dialogType == DialogType.sim || dialogType == DialogType.simNao) || (dialogType == DialogType.ok || dialogType == DialogType.okCancel);
-  }
-  static bool _showNegativeButton(DialogType dialogType) {
-    return (dialogType == DialogType.nao || dialogType == DialogType.simNao) || (dialogType == DialogType.cancel || dialogType == DialogType.okCancel);
+    return (dialogType == DialogType.sim || dialogType == DialogType.simNao) ||
+        (dialogType == DialogType.ok || dialogType == DialogType.okCancel);
   }
 
-  static Future<void> popupPostPerfil(BuildContext context, PostPerfil item) async {
+  static bool _showNegativeButton(DialogType dialogType) {
+    return (dialogType == DialogType.nao || dialogType == DialogType.simNao) ||
+        (dialogType == DialogType.cancel || dialogType == DialogType.okCancel);
+  }
+
+  static Future<void> popupPostPerfil(
+      BuildContext context, PostPerfil item) async {
     showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -142,8 +200,6 @@ class DialogBox {
               },
             ),
           );
-        }
-    );
+        });
   }
-
 }
